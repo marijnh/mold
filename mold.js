89ae15f38168dfbe82e5b8186ece1403e1279939
Marijn Haverbeke <marijnh@gmail.com>
2015-12-11 18:08:32 +0100

    Rewrite for version 2.0

--- mold.js
+++ mold.js
@@ -1,229 +1,163 @@
-var Mold = {};
+module.exports = Mold
 
-(function() {
-  function splitTemplate(template) {
-    var parts = [];
-    function addString(string) {
-      if (string.length)
-        parts.push(string);
-    }
+function Mold(env) {
+  this.env = env || {}
+  this.defs = Object.create(null)
+}
 
-    while (true) {
-      var open = template.search(/[\[<]\?/);
-      if (open == -1) {
-        addString(template);
-        break;
-      }
-      else {
-        addString(template.slice(0, open));
-        var close = template.indexOf("?" + (template.charAt(open) == "<" ? ">" : "]"), open + 2);
-        if (close == -1) throw new Error("'<?' without matching '?>' in template.");
-        var content = template.slice(open + 2, close), match = content.match(/^([\w\/]+)(?:\s+((?:\r|\n|.)+))?$/);
-        if (!match) throw new Error("Template command ('" + content + "') does not follow 'command [arguments]' format.");
-        parts.push({command: match[1], args: match[2]});
-        template = template.slice(close + 2);
-      }
-    }
+Mold.prototype.bake = function(name, string) {
+  if (string == null) { string = name; name = null }
+  var template = new Template(string, name)
+  var result = evaluate(compile(template), this)
+  if (name) this.defs[name] = result
+  return result
+}
 
-    return parts;
+var HTMLspecial = {"<": "&lt;", "&": "&amp;", "\"": "&quot;"}
+Mold.prototype.escapeHTML = function(text) {
+  return String(text).replace(/[<&\"]/g, function(ch) {return HTMLspecial[ch]})
+}
+
+var hop = Object.prototype.hasOwnProperty
+Mold.prototype.forEachIn = function(obj, f) {
+  var i = 0
+  for (var n in obj) if (hop.call(obj, n)) f(n, obj[n], i++)
+}
+
+Mold.prototype.dispatch = function(name, arg) {
+  if (!(name in this.defs))
+    throw new Error("Unrecognised template command: '" + name + "'.")
+  return this.defs[name](arg)
+}
+
+
+
+function Template(string, name) {
+  this.string = string
+  this.name = name
+}
+
+Template.prototype.error = function(message, pos) {
+  var line = 1
+  for (var at = 0;;) {
+    var nl = this.string.indexOf("\n", at)
+    if (nl == -1 || nl > pos) break
+    line++
+    at = nl + 1
+  }
+  throw new SyntaxError(message + (this.name ? " at " + this.name + ":" + line : " at line " + line))
+}
+
+function tokenize(template) {
+  var string = template.string, parts = [], pos = 0
+
+  function addString(string) {
+    var before = /^\n\s*/.exec(string)
+    if (before) string = string.slice(before[0].length)
+    var after = /\n\s*$/.exec(string)
+    if (after) string = string.slice(0, after.index)
+    if (string.length) parts.push(string)
   }
 
-  var snippets, snippet;
-  Mold.addSnippet = function addSnippet(f) {
-    if (snippets) {
-      snippets.push(f);
-      return snippets.length - 1;
+  for (;;) {
+    var open = string.indexOf("<<", pos)
+    if (open == -1) {
+      addString(string.slice(pos))
+      return parts
+    } else {
+      addString(string.slice(pos, open))
+      var close = string.indexOf(">>", open + 2)
+      if (close == -1) template.error("Unclosed template tag", open)
+      var tag = /^([\w\/]+)(?:\s+((?:\r|\n|.)+))?$/.exec(string.slice(open + 2, close))
+      if (!tag) template.error("Invalid template tag", open + 2)
+      parts.push({command: tag[1], args: tag[2], pos: open + 2})
+      pos = close + 2
     }
-  };
+  }
+}
 
-  var labels;
-  Mold.setLabel = function setLabel(name) {
-    return function(node) {
-      if (!labels) labels = {};
+function compile(template) {
+  var tokens = tokenize(template)
+  var code = "function($arg) {\nvar __O = ''\n"
+  var stack = [{type: "top", pos: 0}], match
 
-      if (forDepth > 0) {
-        var array = labels[name] || (labels[name] = []);
-        if (array.push) array.push(node);
+  for (var i = 0; i < tokens.length; i++) {
+    var tok = tokens[i]
+    if (typeof tok == "string") {
+      code += "__O += " + JSON.stringify(tok) + "\n"
+      continue
+    }
+
+    switch (tok.command) {
+    case "text": case "t":
+      code += "__O += __M.escapeHTML(" + tok.args + ")\n"
+      break
+
+    case "html": case "h":
+      code += "__O += (" + tok.args + ")\n"
+      break
+
+    case "do": case "d":
+      code += tok.args + ";\n"
+      break
+
+    case "if":
+      stack.push({type: "if", pos: tok.pos})
+      code += "if (" + tok.args + ") {\n"
+      break
+
+    case "elif":
+      if (stack[stack.length - 1].type != "if") template.error("'elif' without matching 'if'", tok.pos)
+      code += "} else if (" + tok.args + ") {\n"
+      break
+
+    case "else":
+      if (stack[stack.length - 1].type != "if") template.error("'else' without matching 'if'", tok.pos)
+      code += "} else {\n"
+      break
+
+    case "/if":
+      if (stack.pop().type != "if") template.error("'/if' without matching 'if'", tok.pos)
+      code += "}\n"
+      break
+
+    case "for":
+      stack.push({type: "for", pos: tok.pos})
+      if (match = tok.args.match(/^([\w\$_]+)(?:,\s*([\w\$_]+))?\s+in\s+((?:\r|\n|.)+)$/)) {
+        code += "__M.forEachIn(" + match[3] + ", function(" + match[1] + ", " +
+          (match[2] || "$dummy") + ", $i) {\n"
+      } else if (match = tok.args.match(/^([\w\$_]+)\s+((?:\r|\n|.)+)$/)) {
+        code += ";(" + match[2] + ").forEach(function(" + match[1] + ", $i) {\n"
+      } else {
+        template.error("Malformed arguments to 'for' form -- expected variable name followed by expression", tok.pos)
       }
-      else {
-        labels[name] = node;
-      }
-    };
-  };
+      break
 
-  Mold.forDepth = 0;
-  var casting = false;
+    case "/for":
+      if (stack.pop().type != "for") template.error("'/for' without matching 'for'", tok.pos)
+      code += "})\n"
+      break
 
-  var HTMLspecial = {"<": "&lt;", "&": "&amp;", "\"": "&quot;"};
-  Mold.escapeHTML = function escapeHTML(text) {
-    return String(text).replace(/[<&\"]/g, function(ch) {return HTMLspecial[ch];});
-  };
-  var JSspecial = {"\"": "\\\"", "\\": "\\\\", "\f": "\\f", "\b": "\\b",
-                   "\n": "\\n", "\t": "\\t", "\r": "\\r", "\v": "\\v"};
-  function escapeString(text) {
-    return String(text).replace(/[\"\\\f\b\n\t\r\v]/g, function(ch) {return JSspecial[ch];});
+    default:
+      code += "__O += __M.dispatch(" + JSON.stringify(tok.command) + ", " + (/^\s*$/.test(tok.args) ? "null" : tok.args) + ")\n"
+    }
   }
 
-  Mold.attachEvent = null;
-  Mold._attachEvent = function(node, eventName, func) {
-    var wrapped = function(event) {func(event || window.event, node);};
-    if (eventName == "enter") {
-      var origFunc = func;
-      func = function(event, node){if ((event.charCode || event.keyCode) == 13) origFunc(event, node);};
-      eventName = "keydown";
-    }
+  if (stack.length > 1) {
+    var bad = stack.pop()
+    template.error("Unclosed " + bad.type + " block in template", bad.pos)
+  }
 
-    if (Mold.attachEvent)
-      Mold.attachEvent(node, eventName, wrapped);
-    else if (node.addEventListener)
-      node.addEventListener(eventName, wrapped, false);
-    else
-      node.attachEvent("on" + eventName, wrapped);
-  };
+  code += "return __O\n}"
+  return code
+}
 
-  Mold.forEach = function forEach(array, f) {
-    for (var i = 0; i < array.length; i++)
-      f(array[i], i);
-  };
-  var hop = Object.prototype.hasOwnProperty;
-  Mold.forEachIn = function forEachIn(obj, f) {
-    var i = 0;
-    for (var n in obj) {
-      if (hop.call(obj, n))
-        f(n, obj[n], i++);
-    }
-  };
-
-  var custom = {};
-  Mold.define = function(name, func) {
-    custom[name] = func;
-  };
-  Mold.dispatchCustom = function(name, arg, output) {
-    if (!custom.hasOwnProperty(name))
-      throw new Error("Unrecognised template command: '" + name + "'.");
-    output.push(custom[name](arg, output));
-  };
-
-  Mold.bake = function bake(template) {
-    var parts = splitTemplate(template);
-    var func = ["[function templateFunction($arg, __output){\nvar __out = __output || [];\n"];
-    var stack = [], match;
-
-    while (parts.length) {
-      var cur = parts.shift();
-      if (typeof cur == "string") {
-        func.push("__out.push(\"" + escapeString(cur) + "\");\n");
-        continue;
-      }
-      switch (cur.command) {
-
-      case "text": case "t":
-        func.push("__out.push(Mold.escapeHTML(" + cur.args + "));\n");
-        break;
-      case "html": case "h":
-        func.push("__out.push(" + cur.args + ");\n");
-        break;
-      case "do": case "d":
-        func.push(cur.args + "\n");
-        break;
-
-      case "if":
-        stack.push("if");
-        func.push("if (" + cur.args + ") {\n");
-        break;
-      case "elif":
-        if (stack[stack.length - 1] != "if") throw new Error("'elif' without matching 'if' in template.");
-        func.push("} else if (" + cur.args + ") {\n");
-        break;
-      case "else":
-        if (stack[stack.length - 1] != "if") throw new Error("'else' without matching 'if' in template.");
-        func.push("} else {\n");
-        break;
-      case "/if":
-        if (stack.pop() != "if") throw new Error("'/if' without matching 'if' in template.");
-        func.push("}\n");
-        break;
-
-      case "for":
-        stack.push("for");
-        if (match = cur.args.match(/^([\w\$_]+)(?:,\s*([\w\$_]+))?\s+in\s+((?:\r|\n|.)+)$/))
-          func.push("Mold.forDepth++;\nMold.forEachIn(" + match[3] + ", function(" + match[1] + ", " +
-                    (match[2] || "$dummy") + ", $i) {\n");
-        else if (match = cur.args.match(/^([\w\$_]+)\s+((?:\r|\n|.)+)$/))
-          func.push("Mold.forDepth++;\nMold.forEach(" + match[2] + ", function(" + match[1] + ", $i) {\n");
-        else
-          throw new Error("Malformed arguments to 'for' form in template -- expected variable name followed by expression.");
-        break;
-      case "/for":
-        if (stack.pop() != "for") throw new Error("'/for' without matching 'for' in template.");
-        func.push("});\nMold.forDepth--;\n");
-        break;
-
-      case "event":
-        if (match = cur.args.match(/^(\w+)\s+((?:\r|\n|.)+)$/))
-          func.push("__out.push(\"<var class=\\\"__mold \" + Mold.addSnippet(function(__node){Mold._attachEvent(__node, \"" + 
-                    match[1] + "\", function($event, $node) {\n" + match[2] + "\n});}) + \"\\\"></var>\");\n");
-        else
-          throw new Error("Malformed arguments to 'event' form in template -- expected event name followed by handler body.");
-        break;
-      case "run": case "r":
-        func.push("__out.push(\"<var class=\\\"__mold \" + Mold.addSnippet(function($node){" + cur.args + "}) + \"\\\"></var>\");\n");
-        break;
-      case "label": case "l":
-        func.push("__out.push(\"<var class=\\\"__mold \" + Mold.addSnippet(Mold.setLabel(\"" + escapeString(cur.args) +
-                  "\")) + \"\\\"></var>\");\n");
-        break;
-      case "call":
-        var f = cur.args, arr = f.indexOf("->"), arg = "null";
-        if (arr != -1) {arg = f.slice(arr + 2); f = f.slice(0, arr);}
-        func.push(f + "(" + arg + ", __out);");
-        break;
-
-      default:
-        func.push("Mold.dispatchCustom(\"" + escapeString(cur.command) + "\", " + (/^\s*$/.test(cur.args) ? "null" : cur.args) + ", __out);\n");
-      }
-    }
-    if (stack.length) throw new Error("Unclosed blocks in template (" + stack.join() + ").");
-
-    func.push("return __output ? \"\" : __out.join(\"\");\n}]");
-    // The brackets are there to work around some weird IE6 behaviour.
-    return (0,eval)(func.join(""))[0];
-  };
-
-  Mold.cast = function cast(target, mold, arg) {
-    if (casting) throw new Error("Mold.cast must not be called recursively.");
-
-    snippets = [], snippet = 0, labels = null, forDepth = 0, casting = true;
-    try {
-      target.innerHTML = mold(arg);
-      var varTags = target.getElementsByTagName("VAR"), array = [];
-      // Copy tags into array -- FF modifies the varTags collection when you delete nodes in it.
-      for (var i = 0; i < varTags.length; i++)
-        array.push(varTags[i]);
-      for (var i = 0; i < array.length; i++) {
-        var varTag = array[i], match = varTag.className.match(/^__mold (\d+)$/);
-        if (match) {
-          var prev = varTag.previousSibling;
-          while (prev && prev.nodeType == 3) prev = prev.previousSibling;
-          snippets[match[1]](prev || varTag.parentNode);
-          varTag.parentNode.removeChild(varTag);
-        }
-      }
-
-      var result = labels;
-    }
-    finally {
-      labels = snippets = null;
-      casting = false;
-    }
-    return result;
-  };
-
-  Mold.castAppend = function castAppend(target, mold, arg) {
-    var temp = target.ownerDocument.createElement("DIV");
-    var result = Mold.cast(temp, mold, arg);
-    while (temp.firstChild)
-      target.appendChild(temp.firstChild);
-    return result;
-  };
-})();
+function evaluate(code, mold) {
+  var ctx = {__mold: mold}
+  var prelude = "var __M = __CTX.__mold;\n";
+  for (var prop in mold.env) if (mold.env.hasOwnProperty(prop)) {
+    prelude += "var " + prop + " = __CTX." + prop + "\n"
+    ctx[prop] = mold.env[prop]
+  }
+  return new Function("__CTX", prelude + "return " + code)(ctx)
+}
--- test.js
+++ test.js
@@ -1,51 +1,50 @@
-var m = require("./mold.node.js"), tests = [];
+var mold = require("./mold"), m = new mold, tests = []
 
-simple("escape", "a <?t $arg?> b", "<&", "a &lt;&amp; b");
-simple("noescape", "a <?h $arg?> b", "<>", "a <> b");
+simple("escape", "a <<t $arg>> b", "<&", "a &lt;&amp; b");
+simple("noescape", "a <<h $arg>> b", "<>", "a <> b");
 
 simple("array",
-       "a <?for e $arg?>(<?t e?>)<?/for?> b", [1, 2, 3],
+       "a <<for e $arg>>(<<t e>>)<</for>> b", [1, 2, 3],
        "a (1)(2)(3) b");
 simple("obj",
-       "a <?for p, v in $arg?><?t p?>=<?t v?><?/for?> b", {x: 10, y: 20},
+       "a <<for p, v in $arg>><<t p>>=<<t v>><</for>> b", {x: 10, y: 20},
        "a x=10y=20 b");
 simple("array_$i",
-       "a <?for e $arg?>(<?t e?>, <?t $i?>)<?/for?> b", [1, 2, 3],
+       "a <<for e $arg>>(<<t e>>, <<t $i>>)<</for>> b", [1, 2, 3],
        "a (1, 0)(2, 1)(3, 2) b");
 simple("obj_$i",
-       "a <?for p, v in $arg?><?if $i?>, <?/if?><?t p?>=<?t v?><?/for?> b", {x: 10, y: 20},
+       "a <<for p, v in $arg>><<if $i>>, <</if>><<t p>>=<<t v>><</for>> b", {x: 10, y: 20},
        "a x=10, y=20 b");
 
 test("if", function() {
-  var tmpl = m.bake("a <?if $arg?>foo<?/if?><?if !$arg?>bar<?/if?> b");
+  var tmpl = m.bake("a <<if $arg>>foo<</if>><<if !$arg>>bar<</if>> b");
   eq(tmpl(true), "a foo b");
   eq(tmpl(false), "a bar b");
 });
 test("else", function() {
-  var tmpl = m.bake("a <?if $arg?>foo<?else?>bar<?/if?> b");
+  var tmpl = m.bake("a <<if $arg>>foo<<else>>bar<</if>> b");
   eq(tmpl(true), "a foo b");
   eq(tmpl(false), "a bar b");
 });
 test("elif", function() {
-  var tmpl = m.bake("a <?if $arg==1?>foo<?elif $arg==2?>bar<?else?>quux<?/if?> b");
+  var tmpl = m.bake("a <<if $arg==1>>foo<<elif $arg==2>>bar<<else>>quux<</if>> b");
   eq(tmpl(1), "a foo b");
   eq(tmpl(2), "a bar b");
   eq(tmpl(3), "a quux b");
 });
 
 test("ctx", function() {
-  var set, ctx = {a: 100, b: function(x){set = x;}};
-  var tmpl = m.bake("<?t a?><?do b(5);?>x", ctx);
+  var m = new mold({a: 100, b: function(x){set = x;}})
+  var set
+  var tmpl = m.bake("<<t a>><<do b(5);>>x");
   eq(tmpl(), "100x");
   eq(set, 5);
-  ctx.a = 200;
-  eq(tmpl(), "200x");
 });
 
 test("define", function() {
-  m.define("paren", function(a) {return "(" + a + ")";});
-  m.define("sub", m.bake("[<?t $arg?>]"));
-  eq(m.bake("<?paren 10?><?sub 20?>")(), "(10)[20]");
+  m.defs.paren = function(a) {return "(" + a + ")"}
+  m.bake("sub", "[<<t $arg>>]")
+  eq(m.bake("<<paren 10>><<sub 20>>")(), "(10)[20]");
 });
 
 // DRIVER CODE
@@ -85,7 +84,7 @@ function runTests() {
     console.log(failures.length + " failure" + (failures.length - 1 ? "s:" : ":"));
     failures.forEach(function(fail) {
       console.log(" " + fail.name + (fail.condition instanceof Failure ? " failed: " : " threw: ") +
-                  fail.condition.toString());
+                  fail.condition.stack);
     });
   }
 }
